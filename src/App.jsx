88174d8e4ab85e3f271698949dc1a78723a3ad81
Leandro <leandrocordoba318@gmail.com>
2025-08-27 14:37:51 -0300

    adicion de formularios de inicio de sesion y registro

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,18 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import AppRouter from "./routes/AppRouter";
 import "sweetalert2/dist/sweetalert2.min.css";
+import FormRegister from "./components/auth/FormRegister";
+import FormLogin from "./components/auth/FormLogin";
 
 const App = () => {
   return(
   <>
     <Header></Header>
+    <FormLogin></FormLogin>
+    <br /><br />
+    <hr />
+    <br /><br />
+    <FormRegister></FormRegister>
     <AppRouter></AppRouter>;
     <Footer></Footer>
   </>);
