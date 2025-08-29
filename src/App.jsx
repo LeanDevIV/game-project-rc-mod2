59167a56790ea9b162b774565f3fb351b9b05d2e
Leandro <leandrocordoba318@gmail.com>
2025-08-29 16:09:27 -0300

    Adición de componente para modificar datos de juegos y custom hook de localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import AppRouter from "./routes/AppRouter";
 import "sweetalert2/dist/sweetalert2.min.css";
+import AdminTable from "./components/AdminTable";
 
 const App = () => {
   return(
   <>
     <Header></Header>
+    <AdminTable></AdminTable>
     <AppRouter></AppRouter>;
     <Footer></Footer>
   </>);
