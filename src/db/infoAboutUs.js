440b8a6bfa66b090b55e5e5c453da6501c9445b7
isaiasromano17 <isaiasromano745gmail.com>
2025-09-08 18:01:54 -0300

    feat: agregamos pagina acerca de nosotros

---
+++ src/db/infoAboutUs.js
@@ -0,0 +1,38 @@
+const infoAboutUs = [
+  {
+    nombre: "Isaias Romano",
+    imagen: "https://avatars.githubusercontent.com/u/199970736?v=4",
+    github: "https://github.com/isaiasromano17",
+    correo: "isaiasromano745@gmil.com",
+    linkedIn: "https://www.linkedin.com/in/isaias-romano-470773376/",
+  },
+  {
+    nombre: "Leandro Córdoba",
+    imagen: "https://avatars.githubusercontent.com/u/123712000?v=4",
+    github: "https://github.com/LeanDevIV",
+    correo: "leandrocordoba318@gmail.com",
+    linkedIn: "https://www.linkedin.com/in/leandro-c%C3%B3rdoba/",
+  },
+  {
+    nombre: "Luciano Mayorga",
+    imagen: "https://avatars.githubusercontent.com/u/209563119?v=4",
+    github: "-",
+    correo: "-",
+    linkedIn: "-",
+  },
+  {
+    nombre: "Mario Lazo",
+    imagen: "-",
+    github: "-",
+    correo: "",
+    linkedIn: "-",
+  },
+  {
+    nombre: "Alvaro Agustin ",
+    imagen: "https://avatars.githubusercontent.com/u/209853863?v=4",
+    github: "-",
+    correo: "alv2162@gmail.com",
+    linkedIn: "",
+    }
+];
+export default infoAboutUs;
